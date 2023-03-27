63abc8a430636d2dd0a4224050f8977d85ad880e
Tiago Rigoleto <tiagomissiatorigoleto@Tiagos-MacBook-Pro.local>
2023-03-27 11:25:45 -0300

    with migration

--- app/src/main/java/com/tiagomissiato/playgrounddatastore/DataStoreApp.kt
+++ app/src/main/java/com/tiagomissiato/playgrounddatastore/DataStoreApp.kt
@@ -1,7 +1,6 @@
 package com.tiagomissiato.playgrounddatastore
 
 import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
 
-@HiltAndroidApp
+
 class DataStoreApp: Application()
--- app/src/main/java/com/tiagomissiato/playgrounddatastore/MainActivity.kt
+++ app/src/main/java/com/tiagomissiato/playgrounddatastore/MainActivity.kt
@@ -1,11 +1,11 @@
 package com.tiagomissiato.playgrounddatastore
 
+import android.content.Context
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -14,24 +14,28 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.tiagomissiato.playgrounddatastore.customdata.Settings
-import com.tiagomissiato.playgrounddatastore.customdata.SettingsDataSource
-import com.tiagomissiato.playgrounddatastore.preferece.PreferenceDataSource
+import androidx.compose.ui.unit.dp
+import androidx.core.content.edit
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.SharedPreferencesMigration
+import androidx.datastore.preferences.core.*
+import androidx.datastore.preferences.preferencesDataStore
 import com.tiagomissiato.playgrounddatastore.ui.theme.PlaygroundDatastoreTheme
-import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.launch
-import javax.inject.Inject
 
-@AndroidEntryPoint
+val Context.dataStore: DataStore<Preferences> by preferencesDataStore(
+    name = "datastore_preferences",
+    produceMigrations = { context ->
+        listOf(SharedPreferencesMigration(context, "shared_preference"))
+    }
+)
+
 class MainActivity : ComponentActivity() {
 
-
-
-    @Inject
-    lateinit var dataStore: PreferenceDataSource
-
-    @Inject
-    lateinit var settingsStore: SettingsDataSource
+    private val sharedPreference by lazy {
+        getSharedPreferences("shared_preference", MODE_PRIVATE)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -51,7 +55,7 @@ class MainActivity : ComponentActivity() {
                             }
                         }, {
                             scope.launch {
-                                saveSettings()
+                                readPreference()
                             }
                         }
                     )
@@ -60,52 +64,53 @@ class MainActivity : ComponentActivity() {
         }
     }
 
-    private suspend fun saveSettings() {
-        val newValues = Settings(
-                5,
-                "String",
-                listOf("1", "2")
-            )
-        settingsStore.save(newValues)
-        val settingsData = settingsStore.readSettings()
-        Log.i("DEBUG", "intParam ${settingsData.intParam}")
-        Log.i("DEBUG", "StringParam ${settingsData.stringParam}")
-        settingsData.listParam.forEachIndexed{ index, value ->
-            Log.i("DEBUG", "listParam[$index] $value")
+    private fun saveSharedPreference() {
+        sharedPreference.edit {
+            putInt("SharedIntValue", 199)
+            putString("SharedStringValue", "Migrated from shared preference")
+            putBoolean("SharedBooleanValue", false)
         }
     }
 
     private suspend fun savePreference() {
-        dataStore.saveInt("prefKeyInt", 5)
-        dataStore.saveString("prefKeyString", "6")
-        dataStore.saveBoolean("prefKeyBoolean", false)
+        val intKey = intPreferencesKey("intKey")
+        val stringKey = stringPreferencesKey("stringKey")
 
-        Log.i("DEBUG", "prefKeyInt ${dataStore.readInt("prefKeyInt")}")
-        Log.i("DEBUG", "prefKeyInt ${dataStore.readString("prefKeyString")}")
-        Log.i("DEBUG", "prefKeyInt ${dataStore.readBoolean("prefKeyBoolean")}")
+        dataStore.edit {
+            it[intKey] = 10
+            it[stringKey] = "My preference string"
+        }
+    }
 
-//        dataStore.edit {
-//            it[intPreferencesKey("prefKeyInt")] = 5
-//            it[stringPreferencesKey("prefKeyString")] = "5"
-//            it[booleanPreferencesKey("prefKeyBoolean")] = true
-//        }
-//        dataStore.data.collect {
-//            Log.i("DEBUG", "prefKeyInt ${it[intPreferencesKey("prefKeyInt")]}")
-//            Log.i("DEBUG", "prefKeyString ${it[stringPreferencesKey("prefKeyString")]}")
-//            Log.i("DEBUG", "prefKeyBoolean ${it[booleanPreferencesKey("prefKeyBoolean")]}")
-//        }
+    private suspend fun readPreference() {
+        val intKey = intPreferencesKey("intKey")
+        val stringKey = stringPreferencesKey("stringKey")
+
+        val intSharedPref = intPreferencesKey("SharedIntValue")
+        val stringSharedPref = stringPreferencesKey("SharedStringValue")
+        val booleanSharedPref = booleanPreferencesKey("SharedBooleanValue")
+
+        val pref = dataStore.data.first()
+        Log.i("DEBUG", "-----")
+        Log.i("DEBUG", "int original DataStore value: ${pref[intKey]}")
+        Log.i("DEBUG", "string original DataStore value: ${pref[stringKey]}")
+        Log.i("DEBUG", "-----")
+        Log.i("DEBUG", "int migrated from SharedPref value: ${pref[intSharedPref]}")
+        Log.i("DEBUG", "string migrated from SharedPref value: ${pref[stringSharedPref]}")
+        Log.i("DEBUG", "boolean migrated from SharedPref value: ${pref[booleanSharedPref]}")
+        Log.i("DEBUG", "-----")
     }
 }
 
 @Composable
-fun Greeting(onSavePreference: () -> Unit, onSaveSettings: () -> Unit) {
-    Row(modifier = Modifier) {
+fun Greeting(onSavePreference: () -> Unit, readPreference: () -> Unit) {
+    Column(modifier = Modifier) {
         Button(onClick = { onSavePreference() }) {
             Text(text = "Save preference")
         }
-
-        Button(onClick = { onSaveSettings() }) {
-            Text(text = "Save settings")
+        Spacer(modifier = Modifier.width(8.dp))
+        Button(onClick = { readPreference() }) {
+            Text(text = "Read from preference")
         }
     }
 }
